e9e355fcc6e615c114f98d2656ef36647b776e34
damounayman <aymandamoun@gmail.com>
2020-10-02 14:24:18 +0200

    complet week 3

--- Logistic Regression/ex2/costFunctionReg.m
+++ Logistic Regression/ex2/costFunctionReg.m
@@ -20,7 +20,12 @@ grad = zeros(size(theta));
 
 
 
+J = ( (1 / m) * sum(-y'*log(sigmoid(X*theta)) - (1-y)'*log( 1 - sigmoid(X*theta))) ) + (lambda/(2*m))*theta(2:length(theta))'*theta(2:length(theta)) ;
 
+temp = theta;
+temp(1) = 0;
+
+grad = ((1 / m) * (sigmoid(X*theta) - y)' * X) + lambda / m * temp';
 
 % =============================================================
 
